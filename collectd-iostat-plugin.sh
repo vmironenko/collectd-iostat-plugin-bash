3aacf029497a863f0e02680dc7976a2f02ac5217
VMironenko <vmironenko@thumbtack.net>
2015-08-31 14:06:29 +0600

    Update collectd-iostat-plugin.sh

--- collectd-iostat-plugin.sh
+++ collectd-iostat-plugin.sh
@@ -7,8 +7,9 @@ INTERVAL="${COLLECTD_INTERVAL:-1}"
 INTERVAL="`echo $INTERVAL|sed s'#\..*##'`"
 HOSTNAME="`echo $HOSTNAME|sed 's#\.#_#g'`"
 
-iostat_disk() {
-iostat -dxk 1 | awk -v HOSTNAME="$HOSTNAME" -v interval="$INTERVAL" '!/~|Linux|Time:|avg-cpu|Device|^$/{
+func_iostat() {
+iostat -cdxk "$INTERVAL" | awk -v HOSTNAME="$HOSTNAME" -v interval="$INTERVAL" '!/~|Linux|Time:|avg-cpu|Device|^$/{
+if (NF==12){
 # device-1, rrqm_se-2,  wrqm_sec-3, r_s-4, w_s-5, rsec-6, wse-7, avgrq_s-8, avgqu_sz-9, await-10, svctm-11,  util-12
             print "PUTVAL "  HOSTNAME  "/disk_iostat/gauge-"  $1  "/rrqm"    " interval=" interval  " N:"  $2 ;
             print "PUTVAL "  HOSTNAME  "/disk_iostat/gauge-"  $1  "/wrqm"    " interval=" interval  " N:"  $3 ;
@@ -21,11 +22,8 @@ iostat -dxk 1 | awk -v HOSTNAME="$HOSTNAME" -v interval="$INTERVAL" '!/~|Linux|T
             print "PUTVAL "  HOSTNAME  "/disk_iostat/gauge-"  $1  "/await"   " interval=" interval  " N:"  $10 ;
             print "PUTVAL "  HOSTNAME  "/disk_iostat/gauge-"  $1  "/svctm"   " interval=" interval  " N:"  $11 ;
             print "PUTVAL "  HOSTNAME  "/disk_iostat/gauge-"  $1  "/util"    " interval=" interval  " N:"  $12 ;
-         }'
-}
-
-iostat_cpu() {
-iostat -c 1 | awk -v HOSTNAME="$HOSTNAME" -v interval="$INTERVAL" '!/~|Linux|Time:|avg-cpu|Device|^$/{
+          }
+if (NF==6){
 #user-1   nice-2 system-3 iowait-4  steal-5   idle-6
             print "PUTVAL "  HOSTNAME  "/cpu_iostat/gauge-cpu/user"   " interval=" interval  " N:"  $1 ;
             print "PUTVAL "  HOSTNAME  "/cpu_iostat/gauge-cpu/nice"   " interval=" interval  " N:"  $2 ;
@@ -34,17 +32,15 @@ iostat -c 1 | awk -v HOSTNAME="$HOSTNAME" -v interval="$INTERVAL" '!/~|Linux|Tim
             print "PUTVAL "  HOSTNAME  "/cpu_iostat/gauge-cpu/steal"  " interval=" interval  " N:"  $5 ;
             print "PUTVAL "  HOSTNAME  "/cpu_iostat/gauge-cpu/idle"   " interval=" interval  " N:"  $6 ;
             print "PUTVAL "  HOSTNAME  "/cpu_iostat/gauge-cpu/util"   " interval=" interval  " N:"  $1 + $3 ;
-        }'
+          }
+  }'
 }
 
-iostat_disk &
-pid_iostat_disk="$!"
 
-iostat_cpu &
-pid_iostat_cpu="$!"
+func_iostat &
+pid_func="$!"
 
-pid_iostat1="`ps -ef|grep "iostat "|grep $pid_iostat_disk|awk '{print $2}'`"
-pid_iostat2="`ps -ef|grep "iostat "|grep $pid_iostat_cpu|awk '{print $2}'`"
 
-trap "kill  $pid_iostat1 $pid_iostat2 $pid_iostat_disk $pid_iostat_cpu; kill -9 $pid_own; exit;" 1 2 15
+pid_iostat="`ps -ef|grep "iostat "|grep $pid_func|awk '{print $2}'`"
+trap "kill  $pid_iostat $pid_func; kill -9 $pid_own; exit;" 1 2 15
 read e
